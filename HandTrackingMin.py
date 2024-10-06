8c6f08d672e9561e1d546278770e72565d6eae84
Lucifer7344 <luciferthedevil7344@gmail.com>
2024-10-06 11:10:40 +0530

    update

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -20,22 +20,18 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
+                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 print(id, cx, cy)
-                if id ==4:
-                    cv2.circle(img, (cx,cy), 15, (250,0,254),cv2.FILLED)
-
-
-            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+                if id == 4:
+                    cv2.circle(img, (cx, cy), 15, (250, 0, 254), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_DUPLEX,3,(255,0,224),3)
-
-
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_DUPLEX, 3, (255, 0, 224), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
